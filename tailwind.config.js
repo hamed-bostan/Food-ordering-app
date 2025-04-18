c89fc30db367f148c1a78986a4caa2c69f7594e2
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-04-18 17:10:55 +0330

    update tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 const rtl = require("tailwindcss-rtl");
 
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"], // adjust paths as needed
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // for App Router
+    "./pages/**/*.{js,ts,jsx,tsx}", // for Pages Router
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ], // adjust paths as needed
   theme: {
     extend: {},
   },
