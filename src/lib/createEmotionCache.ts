df6e3b296972d8e94f7b00251ac16e757b2978bc
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-04-22 10:20:34 +0330

    add types for stylis-prefixer

--- src/lib/createEmotionCache.ts
+++ src/lib/createEmotionCache.ts
@@ -1,6 +1,6 @@
 import createCache from "@emotion/cache";
 import rtlPlugin from "stylis-plugin-rtl";
-import { prefixer } from "stylis";
+import { prefixer } from "stylis-prefixer";
 
 export default function createEmotionCache() {
   return createCache({
