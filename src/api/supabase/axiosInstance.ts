f4756dcd2115ec28be1fd81cf92a5f1ca10997e9
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-04-22 20:04:13 +0330

    change suppabaseURL

--- src/api/supabase/axiosInstance.ts
+++ src/api/supabase/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 export const axiosInstance = axios.create({
   baseURL: `${supabaseUrl}/rest/v1`,
