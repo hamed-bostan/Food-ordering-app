460715551120ffca867580321a75007eaddcbbc9
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-04-25 23:27:19 +0330

    fix: update footer image

--- src/components/common/footer/index.tsx
+++ src/components/common/footer/index.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import ContactForm from "./ContactForm";
 import Navigation from "./Navigation";
+import image1 from "@/assets/images/bannerImages/banner-03.jpg";
 
 export default function Footer() {
   return (
-    <section className="bg-[url('/assets/images/bannerImages/banner-03.jpg')] bg-center bg-cover w-full grid grid-cols-2 px-5 py-4 lg:px-10 2xl:px-28">
+    <section className="relative grid w-full grid-cols-2 px-5 py-4 lg:px-10 2xl:px-28">
+      <Image
+        src={image1}
+        alt="footer background"
+        fill
+        className="object-cover z-[-1]"
+        priority // if it's important to load fast
+      />
       <Navigation />
       <ContactForm />
     </section>
