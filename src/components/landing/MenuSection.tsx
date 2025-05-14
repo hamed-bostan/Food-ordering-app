5b25627251f1527269f522383fb255829f75384c
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-05-14 08:06:30 +0330

    fix(landing): update menuSection with semantic elements

--- src/components/landing/MenuSection.tsx
+++ src/components/landing/MenuSection.tsx
@@ -35,30 +35,30 @@ const menuInformation: MenuItem[] = [
 
 export default function MenuSection() {
   return (
-    <div className="px-5 mb-16 text-[#353535] lg:px-10 2xl:px-28">
-      <h1 className="mb-3 font-bold text-center md:text-xl md:mb-7">
+    <section className="px-5 mb-16 text-[#353535] lg:px-10 2xl:px-28">
+      <h2 className="mb-3 font-bold text-center md:text-xl md:mb-7">
         منوی رستوران
-      </h1>
-      <div className="grid grid-cols-2 place-items-center gap-x-4 md:gap-x-6 gap-y-16 md:grid-cols-4 xl:gap-x-24">
-        {menuInformation.map((item) => {
-          return (
-            <div
-              className="relative flex justify-center w-full h-28 lg:h-36"
+      </h2>
+      <ul className="grid grid-cols-2 place-items-center gap-x-4 md:gap-x-6 gap-y-16 md:grid-cols-4 xl:gap-x-24">
+        {menuInformation.map((item) => (
+            <li
+              className="relative flex justify-center w-full h-28 lg:h-36 list-none"
               key={item.id}
             >
-              <Image
-                src={item.image}
-                alt={item.text}
-                className="object-contain w-full"
-              />
-              <div className="bg-[#417F56] shadow-xl w-full h-20 rounded-sm absolute top-1/2 -z-10 lg:h-24 max-w-56 md:max-w-full"></div>
-              <span className="text-xs w-24 h-8 bg-[#F9F9F9] rounded-sm shadow-md flex items-center justify-center absolute -bottom-1/2 -translate-y-1/2 md:text-base  lg:bottom-0 lg:translate-y-full lg:w-32 lg:h-10">
-                {item.text}
-              </span>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+              <figure className='flex justify-center'>
+                <Image
+                    src={item.image}
+                    alt={item.text}
+                    className="object-contain w-full"
+                />
+                <div className="bg-[#417F56] shadow-xl w-full h-20 rounded-sm absolute top-1/2 -z-10 lg:h-24 max-w-56 md:max-w-full"></div>
+                <figcaption className="text-xs w-24 h-8 bg-[#F9F9F9] rounded-sm shadow-md flex items-center justify-center absolute -bottom-1/2 -translate-y-1/2 md:text-base  lg:bottom-0 lg:translate-y-full lg:w-32 lg:h-10">
+                  {item.text}
+                </figcaption>
+              </figure>
+            </li>
+        ))}
+      </ul>
+    </section>
   );
 }
