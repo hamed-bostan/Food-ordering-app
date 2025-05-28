9c1ab2ca4574d7264dcd6149f2da6fe0d43c4051
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-05-28 14:58:10 +0330

    chore: add some comments

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import Product from "@/models/Product";
 
 export async function GET() {
-  await connectDB();
-  const products = await Product.find();
-  return NextResponse.json(products);
+  await connectDB(); // Connect to MongoDB
+  const products = await Product.find(); // Get all products from the collection
+  return NextResponse.json(products); // Return as JSON
 }
