b90b09293e5c8d38f18646dad16a95db79eae0fa
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-05-31 13:35:39 +0330

    fix: resolve database connection issue

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,4 +1,3 @@
-// lib/mongoose.ts
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
@@ -7,8 +6,21 @@ if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
 
-export const connectToDatabase = async () => {
-  if (mongoose.connection.readyState >= 1) return;
+let cached = (global as any).mongoose;
 
-  return mongoose.connect(MONGODB_URI);
-};
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
