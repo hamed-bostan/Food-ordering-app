d530c6ab345b13db153fe75ea6f1b3ab4510b5af
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-05-31 10:39:57 +0330

    chore: add comments

--- src/app/api/user-profile/route.ts
+++ src/app/api/user-profile/route.ts
@@ -3,8 +3,15 @@ import { UserProfile } from "@/models/UserProfile";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
+  // Connects to the MongoDB database
   await connectDB();
+
+  // Get the data sent from the client (like form input)
   const data = await req.json();
+
+  // Save the data as a new user in the database
   const user = await UserProfile.create(data);
+
+  // Send the saved user back to the client as a response
   return NextResponse.json(user);
 }
