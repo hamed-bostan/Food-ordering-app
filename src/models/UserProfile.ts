6d104c03d0f3a77dbd37ec49448309b9a05e3b84
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-06-06 22:20:32 +0330

    chore: commented userProfile model

--- src/models/UserProfile.ts
+++ src/models/UserProfile.ts
@@ -1,12 +1,12 @@
-import mongoose, { Schema } from "mongoose";
+// import mongoose, { Schema } from "mongoose";
 
-const UserProfileSchema = new Schema({
-  first_name: String,
-  last_name: String,
-  email: String,
-  phone_number: String,
-});
+// const UserProfileSchema = new Schema({
+//   first_name: String,
+//   last_name: String,
+//   email: String,
+//   phone_number: String,
+// });
 
-export const UserProfile =
-  mongoose.models.UserProfile ||
-  mongoose.model("UserProfile", UserProfileSchema);
+// export const UserProfile =
+//   mongoose.models.UserProfile ||
+//   mongoose.model("UserProfile", UserProfileSchema);
