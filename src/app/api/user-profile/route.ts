1a28705468acc8af21916c4e3b8377a947fb368c
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-06-06 22:10:03 +0330

    chore: commented user-profile api

--- src/app/api/user-profile/route.ts
+++ src/app/api/user-profile/route.ts
@@ -1,17 +1,17 @@
-import { connectDB } from "@/lib/mongoose";
-import { UserProfile } from "@/models/UserProfile";
-import { NextResponse } from "next/server";
+// import { connectDB } from "@/lib/mongoose";
+// import { UserProfile } from "@/models/UserProfile";
+// import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  // Connects to the MongoDB database
-  await connectDB();
+// export async function POST(req: Request) {
+//   // Connects to the MongoDB database
+//   await connectDB();
 
-  // Get the data sent from the client (like form input)
-  const data = await req.json();
+//   // Get the data sent from the client (like form input)
+//   const data = await req.json();
 
-  // Save the data as a new user in the database
-  const user = await UserProfile.create(data);
+//   // Save the data as a new user in the database
+//   const user = await UserProfile.create(data);
 
-  // Send the saved user back to the client as a response
-  return NextResponse.json(user);
-}
+//   // Send the saved user back to the client as a response
+//   return NextResponse.json(user);
+// }
