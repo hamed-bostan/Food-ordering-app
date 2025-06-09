9b6c03e329808f99cf3b83f50a7f56deb7501bec
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-06-09 16:25:24 +0330

    chore: commented supabase.ts file

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,14 +1,14 @@
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 
-// Access environment variables
-const supabaseUrl: string = process.env.SUPABASE_URL as string;
-const supabaseKey: string = process.env.SUPABASE_KEY as string;
+// // Access environment variables
+// const supabaseUrl: string = process.env.SUPABASE_URL as string;
+// const supabaseKey: string = process.env.SUPABASE_KEY as string;
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Missing Supabase environment variables");
-}
+// if (!supabaseUrl || !supabaseKey) {
+//   throw new Error("Missing Supabase environment variables");
+// }
 
-// Initialize Supabase client
-const supabase = createClient(supabaseUrl, supabaseKey);
+// // Initialize Supabase client
+// const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+// export default supabase;
