627765d08a1000cc3fefc7c28a561741147b4ca0
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-07-18 11:27:40 +0330

    refactor(userPanel): more organize structure

--- src/app/userPanel/components/logout/index.tsx
+++ src/app/userPanel/components/logout/index.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import CustomButton from "@/components/ui/CustomButton";
-import { Dialog, DialogTitle, DialogContent, Button } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
+import { Dialog, DialogTitle, DialogContent } from "@mui/material";
 import { useLogoutDialog } from "@/app/userPanel/context/LogoutContext";
-import Link from "next/link";
+import TitleContent from "./TitleContent";
+import MainContent from "./MainContent";
 
 export default function Logout() {
   const { isLogoutDialogOpen, closeLogoutDialog } = useLogoutDialog();
@@ -31,55 +30,14 @@ export default function Logout() {
             backgroundColor: "#EDEDED",
           }}
         >
-          <div className="h-14">
-            <p className="text-sm font-medium md:text-base md:font-semibold text-[#353535] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-              خروج
-            </p>
-
-            <Button
-              onClick={closeLogoutDialog}
-              sx={{
-                color: "#717171",
-                position: "absolute",
-                right: 0,
-                top: "50%",
-                transform: "translateY(-50%)",
-              }}
-            >
-              <CloseIcon />
-            </Button>
-          </div>
+          <TitleContent />
         </DialogTitle>
         <DialogContent
           sx={{
             padding: 0,
           }}
         >
-          <div className="px-6 py-4 lg:pt-8 lg:pb-6 lg:px-14">
-            <p className="text-[#353535] text-xs text-center mb-8 md:text-sm">
-              آیا مایل به خروج از حساب کاربری خود هستید؟
-            </p>
-            <div className="flex gap-x-4">
-              <CustomButton
-                onClick={closeLogoutDialog}
-                disableElevation
-                sx={{
-                  width: "100%",
-                  fontSize: { xs: "12px", lg: "14px" },
-                  height: { xs: "32px", lg: "38px" },
-                }}
-              >
-                بازگشت
-              </CustomButton>
-              <Link
-                role="button"
-                href="/api/auth/signout?callbackUrl=/"
-                className="bg-[#FFF2F2] text-[#C30000] w-full text-xs md:text-sm hover:bg-[#C30000] hover:text-[#FFF] rounded flex items-center justify-center px-4"
-              >
-                خروج
-              </Link>
-            </div>
-          </div>
+          <MainContent />
         </DialogContent>
       </Dialog>
     </div>
