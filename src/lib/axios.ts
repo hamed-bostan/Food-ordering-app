4d51e1c57081896f4989a6ca85544af4e86c622e
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-09-11 00:15:36 +0330

    fix axios instance

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  withCredentials: true, // ✅ send cookies automatically
+  withCredentials: true,
 });
 
 // Optional: response interceptor for logging errors
