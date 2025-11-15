161ef4c3bb865a47bd700d2d5303bf2641ba7607
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-11-15 19:38:20 +0330

    chore: update next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,18 @@
 const nextConfig = {
   output: "standalone",
   images: {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "avatars.githubusercontent.com",
-          pathname: "/**",
-        },
-        {
-          protocol: "https",
-          hostname: "ourbsjovbidvokapxslo.supabase.co",
-          pathname: "/storage/v1/object/public/**",
-        },
-      ],
-    },
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ourbsjovbidvokapxslo.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
   },
 };
 
