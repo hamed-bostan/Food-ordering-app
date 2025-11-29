34da8e9458017750926855b30a0ecf6fbb716fea
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-11-29 11:44:11 +0330

    feat(layout): add link for manage orders

--- src/presentation/features/admin/shared/AdminHeader.tsx
+++ src/presentation/features/admin/shared/AdminHeader.tsx
@@ -5,9 +5,10 @@ export default function AdminHeader() {
     <div className="flex gap-6">
       <Link href="/manage/users">User management</Link>
       <Link href="/manage/testimonials">Testimonials management</Link>
+      <Link href="/create/testimonials">Create testimonials</Link>
       <Link href="/manage/products">Products management</Link>
       <Link href="/create/products">Create products</Link>
-      <Link href="/create/testimonials">Create testimonials</Link>
+      <Link href="/manage/orders">manage orders</Link>
     </div>
   );
 }
